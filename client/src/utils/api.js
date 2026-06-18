3853fa75ec870c69680d6ed4ad73eb53123dd47a
Akande Samad <akandesamad26@gmail.com>
2026-06-18 16:34:36 +0000

    fixed frontend url properly

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://roxyez-api.onrender.com",
+  baseURL: "https://roxyez-api.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
